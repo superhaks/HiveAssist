a16743f7f7c82276f212672ff3381914f8c77f8f
superhaks <hazim.shaikh2919@gmail.com>
2023-07-16 11:56:56 -0700

    Webcam operational

--- Dash.py
+++ Dash.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import pandas as pd
+from camera_input_live import camera_input_live
+from streamlit_webrtc import webrtc_streamer
+
 
 # Adding Header / title of the Dash
 st.title('HiveAssist') 
@@ -8,6 +11,8 @@ st.title('HiveAssist')
 #Adding container for Video Play box in the centre 
 
 #add video playback aligned in the centre
-
+#image = camera_input_live()
+#st.image(30)
+webrtc_streamer(key="test")
 #Create button for facial recognition 
 
