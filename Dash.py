d66f227b43e44a626787d7208d8dc0015eaf3f02
superhaks <hazim.shaikh2919@gmail.com>
2023-07-16 11:15:35 -0700

    Making a framework with comments

--- Dash.py
+++ Dash.py
@@ -1,3 +1,13 @@
 import streamlit as st
 import pandas as pd
-st.title('HiveAssist')
+
+# Adding Header / title of the Dash
+st.title('HiveAssist') 
+
+
+#Adding container for Video Play box in the centre 
+
+#add video playback aligned in the centre
+
+#Create button for facial recognition 
+
