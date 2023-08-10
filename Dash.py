99f9724b5ddf82e3fb8ca08556e07e1a807d6691
superhaks <hazim.shaikh2919@gmail.com>
2023-08-09 20:10:34 -0700

    Integration 1

--- Dash.py
+++ Dash.py
@@ -4,10 +4,10 @@ from camera_input_live import camera_input_live
 from streamlit_webrtc import webrtc_streamer
 import av
 import cv2
-
+import GazeTracking
 # Adding Header / title of the Dash
 st.title('HiveAssist') 
-
+from deepface import DeepFace
 
 #Adding container for Video Play box in the centre 
 
@@ -16,11 +16,36 @@ st.title('HiveAssist')
 #st.image(30)
 #webrtc_streamer(key="test")
 
+#gaze = GazeTracking()
+
+backend = [ 
+    'opencv',
+    'ssd',
+    'dlib',
+    'mtcnn',
+    'retinaface',
+    'mediapipe',
+    'yolov8',
+    'yunet',
+]
+
 class ProcessedVideo:
     def rec(self,frame):
-        frm = frame.to_ndarray(format="bgr24")
+        vframe = frame.to_ndarray(format="bgr24")
+        
+        objs = DeepFace.analyze(img_path=vframe,actions=['emotion'])
+        #while True:
+            #gaze.refresh(vframe)
+           # new_frame = gaze.annotated_frame()
+           # text = ""
 
-        return av.VideoFormat.from_ndarray(frm, format="bgr24")
+           # if gaze.is_right():
+             #   text = "Right"
+           # elif gaze.is_left():
+           #     text = "Left"
+           # elif gaze.is_center():
+           #     text = "Center"
+        return av.VideoFormat.from_ndarray(vframe ,format="bgr24")
 
 
 webrtc_streamer(key="test",video_processor_factory= ProcessedVideo)
