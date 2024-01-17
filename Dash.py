b6a4e7ccb75357b4e16455b4bf4b4b3c164c9ab3
superhaks <hazim.shaikh2919@gmail.com>
2024-01-16 22:03:49 -0800

    Added UI

--- Dash.py
+++ Dash.py
@@ -18,7 +18,14 @@ b.title("Hive Monitor")
 #Putting a divider in between the Header and the rest of the page
 st.divider()
 
-features, camera_in, selector = st.columns(3)
+#creating columns to add individual features on that part of the UI
+features, camera_in, selector = st.columns([0.2, 0.6, 0.2])
+
+#creating a multiselect for all the feature options available options
+options = features.multiselect(
+    "Select the features you want to enable :",
+    ["Face Detection", "Eye Tracking", "Emotion Detection"] 
+)
 
 
 #adding sidebar to the page
