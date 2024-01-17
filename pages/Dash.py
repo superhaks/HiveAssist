538350da2912aa20f031db0ab0de63f0160b3b47
superhaks <hazim.shaikh2919@gmail.com>
2024-01-16 21:04:32 -0800

    Changed the UI for index

--- pages/Dash.py
+++ pages/Dash.py
@@ -1,3 +1,17 @@
 import deepface
 from deepface import DeepFace
+
+import streamlit as st
+
+#setting up the page icon
+st.set_page_config(
+    page_title="HiveAssist",
+    page_icon=":honeybee:"
+)
+
+#providing title for the page
+st.title("Hive-Monitor")
+#adding sidebar to the page
+st.sidebar.success("Select a page above.")
+
 DeepFace.stream("pages/FaceData","VGG-Face","opencv","euclidean_l2")
