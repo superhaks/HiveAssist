1aa0c4e750ab2c008ba441011f23820693785c06
superhaks <hazim.shaikh2919@gmail.com>
2024-01-16 22:03:10 -0800

    Added UI, checkbox,column layout

--- Dash.py
+++ Dash.py
@@ -1,16 +1,26 @@
 import deepface
 from deepface import DeepFace
-
 import streamlit as st
 
-#setting up the page icon
+#setting up the page icon and page layout
 st.set_page_config(
     page_title="HiveAssist",
-    page_icon=":honeybee:"
+    page_icon=":honeybee:",
+    layout="wide"
 )
 
+#Dividing the app interface into columns
+a, b, c = st.columns(3)
+
 #providing title for the page
-st.title("Hive-Monitor")
+b.title("Hive Monitor")
+
+#Putting a divider in between the Header and the rest of the page
+st.divider()
+
+features, camera_in, selector = st.columns(3)
+
+
 #adding sidebar to the page
 st.sidebar.success("Pages")
 
