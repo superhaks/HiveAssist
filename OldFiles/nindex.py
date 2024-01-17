28d7d45e272df08b46e397aa42bf5bca7bf811de
dewonlupin <dewonlupin@gmail.com>
2024-01-16 20:58:50 -0800

    Minor refactor.

--- OldFiles/nindex.py
+++ OldFiles/nindex.py
@@ -1,13 +1,14 @@
 import streamlit as st
 
-#setting up the page icon
+# setting up the page icon
 st.set_page_config(
     page_title="HiveAssist",
     page_icon=":honeybee:"
 )
 
-#providing title for the page
+# providing title for the page
 st.title("Dashboard")
-#adding sidebar to the page
+
+# adding sidebar to the page
 st.sidebar.success("Select a page above.")
 
---
+++ pages/Results.py
@@ -0,0 +1,7 @@
+"""Report components [3 pages PDF report]:
+    1. Heading
+    2. Overall Stats
+    3. Hive Attention Status (HAS) bar chart
+    4.
+    5. Summary
+"""
